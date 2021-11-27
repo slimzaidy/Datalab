2d002245e0444472691d189b1f7437cabc13126d
slimzaidy <askari.zaid@gmail.com>
2021-11-27 19:42:03 +0100

    Update: doc_text

--- 2_Schadcode in Dokumenten/Schad_Dok_XML.py
+++ 2_Schadcode in Dokumenten/Schad_Dok_XML.py
@@ -18,9 +18,10 @@ from sklearn.base import TransformerMixin, BaseEstimator
 
 from bs4 import BeautifulSoup
 import seedir as sd
+from io import BytesIO
 #########################################################################################
 # Define the variables 
-emails = []
+doc_text = []
 labels = []
 #########################################################################################
 # Data preprocessing 
@@ -31,30 +32,60 @@ labels = []
 
 # names = myzip.namelist()
 
-# #names = names[:-1]
 # # file_to_test = names['word/document.xml']
 # Index_Number_For_doc_xml = names.index('word/document.xml')
 # print(Index_Number_For_doc_xml)    
 
 # xml_doc = myzip.open(names[Index_Number_For_doc_xml])
-# data = xml_doc.read()
+# content = xml_doc.read()
 
 
-# soup = BeautifulSoup(data, 'lxml')
+# soup = BeautifulSoup(content, 'lxml')
 # #print(soup.prettify())
 # print(soup.get_text())
 
 # myzip.close()
 
+# Extract the text from each document
 
 train_zip = zipfile.ZipFile("2_Schadcode in Dokumenten/train.zip")
 names_big_files = train_zip.namelist()
 # removing the labels file
 names_big_files = names_big_files[:-1] 
+#names_big_files = names_big_files[:3]
 
 for name in names_big_files:
+    
     tokens = name.split(".")
     labels.append(int(tokens[1]))
 
+    zfiledata = BytesIO(train_zip.read(name))
+    
+    labeled_files_zip = zipfile.ZipFile(zfiledata)
+    sub_names = labeled_files_zip.namelist()
+    Index_Number_For_doc_xml = sub_names.index('word/document.xml')
+    xml_doc = labeled_files_zip.open(sub_names[Index_Number_For_doc_xml])
+    content = xml_doc.read()
+    soup = BeautifulSoup(content, 'lxml')
+    text = soup.get_text()
+    doc_text.append(text)
+
+    labeled_files_zip.close()
 
 train_zip.close()
+
+print(labels[0])
+print(doc_text[0])
+
+#########################################################################################
+# Save the text and labels with the names of the file to a csv
+import csv
+
+f_csv = open("doc_text.csv", "w+", newline ='')
+writer = csv.writer(f_csv, quoting=csv.QUOTE_ALL) 
+
+for name_big_file, label, text in zip(names_big_files, labels, doc_text):
+    x = name_big_file + ";" + str(label) + ";" + text
+    writer.writerow([x])
+
+f_csv.close()
