3a077dfb1b9a270509ce40e3ad5f4cf113ef8df2
slimzaidy <askari.zaid@gmail.com>
2022-01-23 15:51:59 +0100

    Update

---
+++ 5_Netzbasierte_Angriffserkennung/main.py
@@ -0,0 +1,17 @@
+
+rez = []
+
+with open("5_Netzbasierte_Angriffserkennung\output.txt") as f:
+    complete_string = ''
+
+    for x in f:
+        x = x.strip("=")
+        complete_string += x
+    complete_string = " ".join(complete_string.split())[136:].strip()
+
+rez.append(complete_string)
+
+print(rez)
+#print(complete_string)
+
+#for /L %i in (1,1,2) do tshark -r nids-train.pcap -q -z follow,tcp,ascii,%i > strings_train\%i_output.txt
